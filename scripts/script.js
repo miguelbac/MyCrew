6661290a3c77993e4f10fac123aa07b95f20e1f3
¨Saul¨ <sauloteromelchor@gmail.com>
2025-06-03 12:48:25 +0200

    Modales iniciales Pablo y Miguel

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Document is ready!");
    
+    //Creación de variables
     const modalMiguel = document.getElementById("modalMiguel");
+    const modales = document.getElementsByClassName("modal");
+    const nombrePablo =document.getElementById("nombrePablo");
     const nombreMiguel =document.getElementById("nombreMiguel");
     const btnCerrar = document.getElementsByClassName("cerrar");
-    nombreMiguel.addEventListener("click",function(){
-        modalMiguel.showModal();
-        console.log("llego aqui");
+    
+    //Abrir modales
+    nombrePablo.addEventListener("click",function(){
+        modalPablo.showModal();
         
     });
-    // 
+
+     nombreMiguel.addEventListener("click",function(){
+        modalMiguel.showModal();
+        
+    });
+    
+    // Funcionalidad de cerrar
     for (cerrar of btnCerrar){
         cerrar.addEventListener("click",function(){
             modalMiguel.close();
         });
     }
-
+     //Cerrar al clickar fuera
+        window.onclick = function(event) {
+        for(modal of modales){
+            if (event.target == modal) {
+                modal.close();
+             }
+        }
+        }
     
 });
