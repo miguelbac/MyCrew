7ac33db3135f6da69e09090c749dba3957a5b077
saulOMgit <sauloteromelchor@gmail.com>
2025-06-04 15:17:39 +0200

    fix bug merge modal

--- scripts/menu-hamburguesa.js
+++ scripts/menu-hamburguesa.js
@@ -1,9 +1,12 @@
-  const toggleBtn = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('.botonera');
 
-  toggleBtn.addEventListener('click', () => {
-    const expanded = toggleBtn.getAttribute('aria-expanded') === 'true';
-    toggleBtn.setAttribute('aria-expanded', !expanded);
-    nav.classList.toggle('active');
-    toggleBtn.classList.toggle('active');
-  });
+  document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.botonera');
+
+    toggleBtn.addEventListener('click', () => {
+      const expanded = toggleBtn.getAttribute('aria-expanded') === 'true';
+      toggleBtn.setAttribute('aria-expanded', !expanded);
+      nav.classList.toggle('active');
+      toggleBtn.classList.toggle('active');
+    });
+  })
