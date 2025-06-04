1c722c16d398211e2960d933b5b37edfd76436f5
saulOMgit <sauloteromelchor@gmail.com>
2025-06-04 14:59:04 +0200

    Modales Funcinales y Responsivo

--- scripts/script.js
+++ scripts/script.js
@@ -53,41 +53,50 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function() {
    
     //Creación de variables
-    const modalMiguel = document.getElementById("modalMiguel");
-    const modales = document.getElementsByClassName("modal");
+    const modales = Array.from(document.getElementsByClassName("modal"));
     const nombrePablo =document.getElementById("nombrePablo");
+    const modalPablo = document.getElementById("modalPablo");
     const nombreMiguel =document.getElementById("nombreMiguel");
+    const modalMiguel = document.getElementById("modalMiguel");
+    const nombrePaula =document.getElementById("nombrePaula");
+    const modalPaula = document.getElementById("modalPaula");
+    const nombreSaul =document.getElementById("nombreSaul");
+    const modalSaul = document.getElementById("modalSaul");
+
     const btnCerrar = document.getElementsByClassName("cerrar");
     
     //Abrir modales
     nombrePablo.addEventListener("click",function(){
-        modalPablo.showModal();
-        
+        modalPablo.showModal();        
     });
 
      nombreMiguel.addEventListener("click",function(){
-        modalMiguel.showModal();
-   console.log("hola");
-        
+        modalMiguel.showModal();        
+    });
+     nombrePaula.addEventListener("click",function(){
+        modalPaula.showModal();        
+    });
+     nombreSaul.addEventListener("click",function(){
+        modalSaul.showModal();        
     });
     
+
     // Funcionalidad de cerrar
-    for (cerrar of btnCerrar){
+    for (const cerrar of btnCerrar){
         cerrar.addEventListener("click",function(){
             for(modal of modales){
                 modal.close();
             }
         });
     }
-    modalPablo.
+   
      //Cerrar al clickar fuera
-        window.onclick = function(event) {
-        for(modal of modales){
-            if (event.target == modal) {
+     window.addEventListener("click", (event) => {
+        for (const modal of modales) {
+            if (event.target === modal) {
                 modal.close();
-             }
+            }
         }
-        }
-    
+    });
 });
 >>>>>>> f953355 (modal casi creado)
