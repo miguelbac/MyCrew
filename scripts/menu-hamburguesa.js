3f5780926361c3de6d535a99c7e1b71545e8ae37
Miguel Ballesteros <mbacebal@gmail.com>
2025-06-04 14:30:40 +0200

    Fusión Paula+Miguel

---
+++ scripts/menu-hamburguesa.js
@@ -0,0 +1,9 @@
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.botonera');
+
+  toggleBtn.addEventListener('click', () => {
+    const expanded = toggleBtn.getAttribute('aria-expanded') === 'true';
+    toggleBtn.setAttribute('aria-expanded', !expanded);
+    nav.classList.toggle('active');
+    toggleBtn.classList.toggle('active');
+  });
